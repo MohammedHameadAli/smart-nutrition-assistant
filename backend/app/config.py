0c13812d0fdf62b2709743c3101fff9e7c8d5ff7
MohammedHameadAli <alihamead07@gmail.com>
2026-03-15 15:24:38 +0530

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,6 +4,6 @@ JWT_SECRET = "supersecretkey"
 JWT_ALGORITHM = "HS256"
 JWT_EXPIRE_MINUTES = 60
 
-MONGO_URI = "mongodb+srv://hameadali08_db_user:Hamead2004@nutritionappcluster.cyioutn.mongodb.net/?appName=Nutritionappcluster"
-DB_NAME = "food_app"
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = "the foods app name"
 
